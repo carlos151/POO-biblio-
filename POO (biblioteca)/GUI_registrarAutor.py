0a2fcbb30faff29aea4e11fb56e7c7f0e5a27d0d
Unknown <cpenas@ic-itcr.ac.cr>
2018-05-24 22:07:38 -0600

    tamaños windows

--- POO (biblioteca)/GUI_registrarAutor.py
+++ POO (biblioteca)/GUI_registrarAutor.py
@@ -25,27 +25,27 @@ class RegistrarAutor:
         self.__fechaLabel.place(x=10,y=160)
         self.__nacionalidadLabel.place(x=10,y=200)
 
-        self.__nombreEntry = Entry(root,width=50)
+        self.__nombreEntry = Entry(root,width=67)
         self.__nombreEntry.place(x=80,y=81)
 
-        self.__idEntry = Entry(root,width=50)
+        self.__idEntry = Entry(root,width=67)
         self.__idEntry.place(x=80,y=121)
 
-        self.__fechaEntry = calendario.Datepicker(self.__root,39)
+        self.__fechaEntry = calendario.Datepicker(self.__root,52)
         self.__fechaEntry.place(x=170,y=161)
         self.__calOriginal = Image.open("res/calendar.png")
         self.__calResized = self.__calOriginal.resize((20, 20), Image.ANTIALIAS)
         self.__cal = ImageTk.PhotoImage(self.__calResized)
 
-        self.__nacionalidadEntry = Entry(root,width=42)
+        self.__nacionalidadEntry = Entry(root,width=56)
         self.__nacionalidadEntry.place(x=115,y=200)
         self.__arrowOriginal = Image.open("res/down_arrow.png")
         self.__arrowResized = self.__arrowOriginal.resize((20,20), Image.ANTIALIAS)
         self.__arrow = ImageTk.PhotoImage(self.__arrowResized)
         self.__nacionalidadBoton = Button(root,image=self.__arrow,font=("Arial","12"),height=20,width=20,command=self.abrirLista)
-        self.__nacionalidadBoton.place(x=460,y=198)
+        self.__nacionalidadBoton.place(x=460,y=195)
 
-        self.__nacionalidadLista = Listbox(root,font=("Arial","12"),height=11,width=37,selectmode=SINGLE)
+        self.__nacionalidadLista = Listbox(root,font=("Arial","12"),height=11,selectmode=SINGLE)
         self.__nacionalidadLista.bind('<<ListboxSelect>>', self.onSelect)
         self.generarNacionalidades()
 
@@ -70,7 +70,7 @@ class RegistrarAutor:
 
 
     def abrirLista(self):
-        self.__nacionalidadLista.place(x=115, y=221)
+        self.__nacionalidadLista.place(x=115, y=219,width=340)
         self.__nacionalidadBoton.configure(command=self.cerrarLista)
 
     def cerrarLista(self):
@@ -92,10 +92,10 @@ class RegistrarAutor:
     def getFecha(self):
         try:
             separarElementos = self.__fechaEntry.get().split("-")
-            año = separarElementos[0]
+            year = separarElementos[0]
             mes = separarElementos[1]
             dia = separarElementos[2]
-            return dia + "/" + mes + "/" + año
+            return dia + "/" + mes + "/" + year
         except:
             return ""
 
